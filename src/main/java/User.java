b3205bd038e10c6dcc97692c56d51a3a6c96d0bb
Your Name <you@example.com>
2024-01-20 15:22:51 +0330

    add needed feild, lombok

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,16 @@
+import lombok.*;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
 public class User {
+
+    private int id;
+    private String name;
+    private String userName;
+    private String email;
+    private String password;
+
 }

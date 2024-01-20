9cdab670f8c93b5892cc03275b29ff75f465e962
Your Name <you@example.com>
2024-01-20 15:39:15 +0330

    add publicMenu

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -1,4 +1,23 @@
 package menu;
 
+import java.util.Scanner;
+
 public class Menu {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+
+    public void publicMenu() {
+        System.out.println("*** welcom ***\nMENU");
+        System.out.println("1-SIGN UP");
+        System.out.println("2-SIGN IN");
+        System.out.println("3-EXIT");
+        System.out.println("Choose your number");
+
+        int number = scanner.nextInt();
+        scanner.nextLine();
+    }
+
+
 }
+

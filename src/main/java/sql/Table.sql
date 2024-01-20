9a030a11779fabef9a16383e732588946ff7c11f
Your Name <you@example.com>
2024-01-20 22:20:46 +0330

    add the other table need and finish

--- src/main/java/sql/Table.sql
+++ src/main/java/sql/Table.sql
@@ -3,9 +3,55 @@ CREATE TABLE IF NOT EXISTS users
 
     id        SERIAL PRIMARY KEY,
     name      VARCHAR,
-    user_name VARCHAR,
-    email     VARCHAR,
+    user_name VARCHAR UNIQUE,
+    email     VARCHAR UNIQUE,
     password  VARCHAR
+
+
+);
+CREATE TABLE IF NOT EXISTS product
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR,
+    create_date VARCHAR,
+    category_id INTEGER,
+    brand_id    INTEGER,
+    FOREIGN KEY (category_id) REFERENCES category (id),
+    FOREIGN KEY (brand_id) REFERENCES brand (id)
+);
+CREATE TABLE IF NOT EXISTS category
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR,
+    description VARCHAR
+
+
+);
+CREATE TABLE IF NOT EXISTS brand
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR,
+    website     VARCHAR,
+    description VARCHAR
+
+
+);
+CREATE TABLE IF NOT EXISTS shareholder
+(
+    id            SERIAL PRIMARY KEY,
+    name          VARCHAR,
+    phone_number  VARCHAR,
+    national_code VARCHAR
+
+
+);
+CREATE TABLE IF NOT EXISTS shareholder_brand
+(
+    brand_id       INTEGER,
+    shareholder_id INTEGER,
+
+    FOREIGN KEY (brand_id) REFERENCES shareholder (id),
+    FOREIGN KEY (shareholder_id) REFERENCES brand (id)
 
 
 );

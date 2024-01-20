c4fcee9c7d99ee9a8ac85aafdc18a2c4f7ff15d5
Your Name <you@example.com>
2024-01-20 16:45:04 +0330

    signup method thats finish

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,4 +1,39 @@
 package service;
 
+import model.User;
+import userrepository.UserRepository;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class UserService {
+
+    private final Scanner scanner = new Scanner(System.in);
+    private final UserRepository userRepository = new UserRepository();
+
+    public UserService() throws SQLException {
+    }
+
+    public void signup() throws SQLException {
+        System.out.println("please enter your name:");
+        String name = scanner.next();
+
+        System.out.println("please enter your username:");
+        String userName = scanner.next();
+
+        System.out.println("please enter your Email:");
+        String email = scanner.next();
+
+        System.out.println("please enter your password:");
+        String password = scanner.next();
+
+        User user = new User(name, userName, email, password);
+        int result = userRepository.registerUser(user);
+
+        if (result == 1)
+            System.out.println(name + "you successfully is signup :)");
+        else
+            System.out.println("something is wrong :/");
+    }
+
 }

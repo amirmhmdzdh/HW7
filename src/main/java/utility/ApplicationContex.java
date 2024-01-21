976090549802e4c414351ba90c0d0ddd66d49073
Your Name <you@example.com>
2024-01-22 01:42:16 +0330

    add Brand repo , service

--- src/main/java/utility/ApplicationContex.java
+++ src/main/java/utility/ApplicationContex.java
@@ -1,8 +1,11 @@
 package utility;
 
 import connection.JdbcConnection;
+import repository.BrandRepository;
+import service.BrandService;
 import service.UserService;
-import userrepository.UserRepository;
+import repository.UserRepository;
+
 import java.sql.Connection;
 
 public class ApplicationContex {
@@ -10,15 +13,25 @@ public class ApplicationContex {
     private static final Connection CONNECTION;
     private static final UserRepository USER_REPOSITORY;
     private static final UserService USER_SERVICE;
+    private static final BrandRepository BRAND_REPOSITORY;
+    private static final BrandService BRAND_SERVICE;
 
     static {
 
         CONNECTION = JdbcConnection.getConnection();
         USER_REPOSITORY = new UserRepository(CONNECTION);
         USER_SERVICE = new UserService(USER_REPOSITORY);
-    }
- public static UserService getUserService(){
-        return USER_SERVICE;
- }
 
+        BRAND_REPOSITORY = new BrandRepository(CONNECTION);
+        BRAND_SERVICE=new BrandService(BRAND_REPOSITORY);
+
+
+    }
+
+    public static UserService getUserService() {
+        return USER_SERVICE;
+    }
+    public static BrandService getBrandService() {
+        return BRAND_SERVICE;
+    }
 }

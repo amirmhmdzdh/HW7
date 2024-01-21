47e82aa27f31af813477c89591a66b8d4a4da636
Your Name <you@example.com>
2024-01-22 01:40:23 +0330

    add 2method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,16 @@ import menu.Menu;
 
 import java.sql.SQLException;
 
+import static menu.Menu.brandMenu;
+import static menu.Menu.categoryMenu;
+
 public class Main {
     public static void main(String[] args) throws SQLException {
 
         Menu menu = new Menu();
         menu.publicMenu();
+        brandMenu();
+        categoryMenu();
     }
 
 }

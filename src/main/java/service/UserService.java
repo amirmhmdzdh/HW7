41c3ce4a9f44449a0beada68aa60c270421c1245
Your Name <you@example.com>
2024-01-23 12:56:29 +0330

    add Valid for some method

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,7 +1,8 @@
 package service;
 
 import model.User;
-import userrepository.UserRepository;
+import repository.UserRepository;
+import utility.InputValidation;
 
 import java.sql.SQLException;
 import java.util.Scanner;
@@ -10,23 +11,49 @@ public class UserService {
 
     private final Scanner scanner = new Scanner(System.in);
     private final UserRepository userRepository;
-    public UserService(UserRepository userRepository){
-    this.userRepository=userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     public void signup() throws SQLException {
         System.out.println("please enter your name:");
         String name = scanner.next();
 
-        System.out.println("please enter your username:");
-        String userName = scanner.next();
 
-        System.out.println("please enter your Email:");
-        String email = scanner.next();
+        String userName;
+        while (true) {
 
-        System.out.println("please enter your password:");
-        String password = scanner.next();
+            System.out.println("please enter your username:");
+            userName = scanner.next();
+            if (InputValidation.isValidUsername(userName))
+                break;
+            else
+                System.out.println("please enter correct username");
+        }
 
+
+        String email;
+        while (true) {
+            System.out.println("please enter your Email:");
+            email = scanner.next();
+
+            if (InputValidation.isValidEmail(email))
+                break;
+            else
+                System.out.println("please enter a correct Email");
+        }
+
+        String password;
+        while (true) {
+
+            System.out.println("please enter your password:");
+            password = scanner.next();
+            if (InputValidation.isValidPassword(password))
+                break;
+            else
+                System.out.println("please enter correct password");
+        }
         User user = new User(name, userName, email, password);
         int result = userRepository.registerUser(user);
 
@@ -49,9 +76,9 @@ public class UserService {
 
         else if (!user.getPassword().equals(password)) {
             System.out.println("please enter correct password");
-        }
-        else
+        } else
             System.out.println("WELCOME " + user.getUserName());
+        System.out.println();
 
     }
 }

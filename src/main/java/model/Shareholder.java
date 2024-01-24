3894adaabfd3e2637e0c532fde816198e9358d7a
Your Name <you@example.com>
2024-01-24 16:57:11 +0330

    change data type and add constructor

--- src/main/java/model/Shareholder.java
+++ src/main/java/model/Shareholder.java
@@ -10,11 +10,25 @@ import lombok.*;
 public class Shareholder {
     private int id;
     private String name;
-    private int phoneNumber;
+    private String phoneNumber;
     private String nationalCode;
+    private int brand;
 
+    public Shareholder(String name, String phoneNumber, int brand) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.nationalCode = nationalCode;
+        this.brand = brand;
+    }
 
-    public Shareholder(String name, int phoneNumber, String nationalCode) {
+    public Shareholder(int id, String name, String phoneNumber, String nationalCode) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.nationalCode = nationalCode;
+        this.id = id;
+    }
+
+    public Shareholder(String name, String phoneNumber, String nationalCode) {
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.nationalCode = nationalCode;

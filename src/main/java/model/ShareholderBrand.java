e09e31193dab169ad17a6d0eb195f66b5fcf9ca8
Your Name <you@example.com>
2024-01-24 13:22:17 +0330

    Create ShareholderBrand and add field

---
+++ src/main/java/model/ShareholderBrand.java
@@ -0,0 +1,18 @@
+package model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ShareholderBrand {
+
+    private int id ;
+    private int shareholder;
+    private int brand;
+
+
+}
+
